43bb434170f2f50a956d72f31f39da1bf5772c3d
Pirmin Kalberer <pka@sourcepole.ch>
2023-10-27 23:57:41 +0200

    Update Benchmark DB to PG 16 / Postgis 3.4

--- data/setup-db.sh
+++ data/setup-db.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 
-psql -c "CREATE ROLE mvtbench SUPERUSER LOGIN PASSWORD 'mvtbench'"
-
-export PGUSER=mvtbench
-make createdb gpkgrestore
-
-psql -c "ALTER ROLE mvtbench NOSUPERUSER"
+make gpkgrestore
